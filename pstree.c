c9970930f14ab3a21db50150bb852937ee0aa6be
tracer <tracer@tracer.org>
2018-03-12 16:42:04 +0800

    >  compile lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- pstree.c
+++ pstree.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <string.h>
+
+struct Proc{
+	int pid, ppid, pgrp; 
+	char name[64];
+};
+
+bool str_is_digit(char *s){
+	bool ok = true;
+	for (int i = 0; i < strlen(s); i++){
+		if (!isdigit(s[i])) {
+			ok = false; break;
+		}
+	}
+	return ok;
+}
+
+void read_proc(char* dir){
+	DIR *dp;
+	struct dirent *dirp;
+	if ((dp = opendir(dir)) == NULL) {
+		printf("Can't open %s\n", dir); 
+		return;
+	}
+	while ((dirp = readdir(dp)) != NULL) {
+		if (!str_is_digit(dirp->d_name)) continue;
+		else printf("%s\n", dirp->d_name);
+	}
+	closedir(dp);
+}
+
 
 int main(int argc, char *argv[]) {
   int i;
@@ -8,5 +43,8 @@ int main(int argc, char *argv[]) {
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]); // specification
+
+  read_proc("/proc");
+
   return 0;
 }
