2dfb4053224d2ca3b2dd7522cbbe8385785e5332
tracer <tracer@tracer.org>
2018-03-12 17:16:03 +0800

    >  compile lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- pstree.c
+++ pstree.c
@@ -28,7 +28,7 @@ bool str_is_digit(char *s){
 }
 
 void add_proc(char* dir){
-	printf("strcat(dir,"/stat"\n");
+	printf("strcat(dir,"/stat")");
 	FILE* fp = fopen(strcat(dir, "/stat"), "r");
 	if (fp) {
 	  // 用fscanf, fgets等函数读取
