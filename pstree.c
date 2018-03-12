7e51821dd5dc2a9b773128ccd2747eb09915973c
tracer <tracer@tracer.org>
2018-03-12 19:18:32 +0800

    >  compile lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- pstree.c
+++ pstree.c
@@ -34,7 +34,7 @@ bool str_is_digit(char *s){
 void add_proc(char* filename){
 	FILE* fp = fopen(filename, "r");
 	if (fp) {
-	  if (fscanf(fp, "%d(%s)%s%d%d", &proc[pn].pid, proc[pn].name, &proc[pn].state, &proc[pn].ppid, &proc[pn].pgrp) > 0){
+	  if (fscanf(fp, "%d (%s) %s%d%d", &proc[pn].pid, proc[pn].name, &proc[pn].state, &proc[pn].ppid, &proc[pn].pgrp) > 0){
 	  	//proc[pn].sz = 0, proc[pn].mxsz = 50, proc[pn].sons = (int *)malloc(50 * sizeof(int));
 	  	printf("%d : %d %s %s %d %d\n", pn,  proc[pn].pid, proc[pn].name, proc[pn].state, proc[pn].ppid, proc[pn].pgrp);
 	  	++pn; puts("OK");;
