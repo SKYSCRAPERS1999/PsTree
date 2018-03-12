81b89a39608b655198b25a731b15bf3ab62ac062
tracer <tracer@tracer.org>
2018-03-12 23:11:53 +0800

    >  compile lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- pstree.c
+++ pstree.c
@@ -137,13 +137,7 @@ int main(int argc, char *argv[]) {
     if (!strcmp(argv[i], "-p") || !strcmp(argv[i], "--show-pids")) show_pid = true;
     else if (!strcmp(argv[i], "-n") || !strcmp(argv[i], "--numeric-sort")) be_sorted = true;
     if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "--version")){
-    	printf("pstree (PSmisc) 22.21\n
-		Copyright (C) 1993-2009 Werner Almesberger and Craig Small\n
-		PSmisc comes with ABSOLUTELY NO WARRANTY.\n
-		This is free software, and you are welcome to redistribute it under\n
-		the terms of the GNU General Public License.\n
-		For more information about these matters, see the files named COPYING.\n
-		Escape~");
+    	printf("pstree (PSmisc) 22.21\nCopyright (C) 1993-2009 Werner Almesberger and Craig Small\nPSmisc comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under\nthe terms of the GNU General Public License.\nFor more information about these matters, see the files named COPYING.\nEscape~");
 		return 0;
     }
   }
