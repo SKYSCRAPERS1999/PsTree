72c80d4dc7e173315931d86d2ba49e717bfbd9ec
tracer <tracer@tracer.org>
2018-03-12 21:51:19 +0800

    >  compile lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- pstree.c
+++ pstree.c
@@ -97,9 +97,9 @@ void dfs_print(){
 }
 
 void dfs(int x, int px, int indent, int* indents, int in){
-	for (int i = 0, j = 0; i < indent && j < in; i++){
-		while (indents[j] < i) j++;
-		if (indents[j] == i) printf("|"), j++;
+	for (int i = 0, j = 0; i < indent; i++){
+		while (j < in && indents[j] < i) j++;
+		if (j < in && indents[j] == i) printf("|"), j++;
 		else printf(" ");
 	}
 
