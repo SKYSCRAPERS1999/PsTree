68ee63680c982aa0f496630b911ebfd3d46ef16d
tracer <tracer@tracer.org>
2018-03-12 17:29:07 +0800

    >  compile lanfangzhou Linux impulse 4.13.0-32-generic #35~16.04.1-Ubuntu SMP Thu Jan 25 10:13:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- pstree.c
+++ pstree.c
@@ -55,7 +55,7 @@ void read_proc(char* dir){
 		if (!str_is_digit(dirp->d_name)) continue;
 		else {
 			printf("%s%s\n", dir, dirp->d_name);
-			char* new_dir = strcat(dir, dirp->d_name);
+			char* new_dir = strcat(dir, (const char *)dirp->d_name);
 			printf("%s", new_dir);
 			//add_proc(new_dir);
 		}
